a96ecd30f86a419e72612e1bd70d9d47c3f38b50
Greg Fyans <greg@screenmedia.co.uk>
2016-02-18 09:21:44 +0000

    Runtime policy works with Umbraco 7.4

--- src/Glimpse7/GlimpseSecurityPolicy.cs
+++ src/Glimpse7/GlimpseSecurityPolicy.cs
@@ -1,20 +1,19 @@
-// Uncomment this class to provide custom runtime policy for Glimpse
-
-using Glimpse.AspNet.Extensions;
+using System.Web;
+using System.Web.Security;
 using Glimpse.Core.Extensibility;
-using Umbraco.Core.Services;
-using Umbraco.Core.Persistence;
+using Umbraco.Core.Security;
 
 namespace Glimpse7
 {
-    public class GlimpseSecurityPolicy : IRuntimePolicy
+    public class GlimpseSecurityPolicy:IRuntimePolicy
     {
         public RuntimePolicy Execute(IRuntimePolicyContext policyContext)
         {
             // You can perform a check like the one below to control Glimpse's permissions within your application.
             // More information about RuntimePolicies can be found at http://getglimpse.com/Help/Custom-Runtime-Policy
-            var currentUser = new UserService(new RepositoryFactory());
-            if (currentUser == null)
+            FormsAuthenticationTicket ticket = new HttpContextWrapper(HttpContext.Current).GetUmbracoAuthTicket();
+
+            if (string.IsNullOrWhiteSpace(ticket?.Name) || ticket.Expired)
             {
                 return RuntimePolicy.Off;
             }
@@ -24,8 +23,8 @@ namespace Glimpse7
 
         public RuntimeEvent ExecuteOn
         {
-            // The RuntimeEvent.ExecuteResource is only needed in case you create a security policy
-            // Have a look at http://blog.getglimpse.com/2013/12/09/protect-glimpse-axd-with-your-custom-runtime-policy/ for more details
+			// The RuntimeEvent.ExecuteResource is only needed in case you create a security policy
+			// Have a look at http://blog.getglimpse.com/2013/12/09/protect-glimpse-axd-with-your-custom-runtime-policy/ for more details
             get { return RuntimeEvent.EndRequest | RuntimeEvent.ExecuteResource; }
         }
     }
